46577ef3a8b1cb3c2404cdf419c595720b930624
OtterSloth <76695983+OtterSloth@users.noreply.github.com>
2022-11-15 10:32:31 -0500

    Add chatbox component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Items from './components/Items'
+import Chatbox from './components/Chatbox'
 
 function App() {
   return (
     <div className='App'>
       <div className='container'>
         <Header />
+
         <main>
           <Hero />
 
           <Items />
         </main>
+
+        <Chatbox />
       </div>
     </div>
   )
