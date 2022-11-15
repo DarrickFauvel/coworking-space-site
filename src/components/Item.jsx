b0bcbe95f334674ab9b4164cd92f6fb5e6070f0b
OtterSloth <76695983+OtterSloth@users.noreply.github.com>
2022-11-15 10:00:48 -0500

    Add item component

---
+++ src/components/Item.jsx
@@ -0,0 +1,18 @@
+const Item = ({ workspace }) => {
+  return (
+    <section className='item'>
+      <img
+        src={`images/${workspace.image}`}
+        className='item-img'
+        alt={workspace.altText}
+      />
+      {workspace.banner && <div class='img-banner'>{workspace.banner}</div>}
+      <div className='caption'>
+        <p>{workspace.caption}</p>
+        <button>Book</button>
+      </div>
+    </section>
+  )
+}
+
+export default Item
