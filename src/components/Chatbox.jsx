689982b90bc3ab8a0bd17261e135de7be141138a
OtterSloth <76695983+OtterSloth@users.noreply.github.com>
2022-11-15 10:32:43 -0500

    Create chatbox component

---
+++ src/components/Chatbox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chatbox = () => {
+  return (
+    <div class='chatbox-bg'>
+      <img src='images/message.png' className='chatbox-img' alt='chatbox' />
+    </div>
+  )
+}
+
+export default Chatbox
